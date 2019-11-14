30275754889f709626ef5c1eef82ddd7b6312b7e
George Acosta <georgeacostar@gmail.com>
2019-11-13 20:36:01 -0800

    add test and revising tests

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,17 +1,18 @@
-import { Triangle } from './../src/triangle.js';
+/* eslint-disable no-undef */
+import { Triangle, addSides, multiplySides } from './../src/triangle.js';
 
 describe('Triangle', () => {
 
   test('should correctly determine whether the input string contains a number', () => {
-    var stringWithNumber = new Triangle("e","e","e")
+    var stringWithNumber = new Triangle("e","e","e");
     expect(stringWithNumber.checkType()).toEqual("this string does not contain a number");
   });
   test('should correctly determine whether the input is a string', () => {
-    var string = new Triangle("2","2","2")
+    var string = new Triangle("2","2","2");
     expect(string.checkType()).toEqual("this is string cheese");
   });
   test('should correctly determine whether the input is a negative number', () => {
-    var negNumber = new Triangle(-8,-3,-3)
+    var negNumber = new Triangle(-8,-3,-3);
     expect(negNumber.checkType()).toEqual("this is a negative number");
   });
   test('should correctly create a triangle object with three lengths', () => {
@@ -27,15 +28,39 @@ describe('Triangle', () => {
   });
 
   test('Should correctly determine whether threee lengths make a scalene triangle', function(){
-    var scalTraingle = new Triangle(4,5,7)
+    var scalTraingle = new Triangle(4,5,7);
     expect(scalTraingle.checkType()).toEqual("scalene triangle");
   });
   test('should correctly determine whether three lengths make an isosceles triangle', () => {
-    var isoTriangle = new Triangle(5,5,7)
+    var isoTriangle = new Triangle(5,5,7);
     expect(isoTriangle.checkType()).toEqual("isosceles triangle");
   });
   test('should correctly determine whether three lengths make an equilateral triangle', () => {
-    var equilTriangle = new Triangle(5,5,5)
+    var equilTriangle = new Triangle(5,5,5);
     expect(equilTriangle.checkType()).toEqual("equilateral triangle");
   });
+
+  describe("addSides", () => {
+    test("receives a triangle, returns the sum of the sides", () => {
+      var equilTriangle = new Triangle(5,5,5);
+
+      var actual = addSides(equilTriangle);
+      var expected = 15;
+
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  describe("multiplySides", () => {
+    test("receives a triangle, returns the product of the sides", () => {
+      var equilTriangle = new Triangle(5,5,6);
+
+      var actual = multiplySides(equilTriangle);
+      var expected = equilTriangle.side1 * equilTriangle.side2 * equilTriangle.side3;
+
+      expect(actual).toEqual(expected);
+    });
+  });
+  
 });
+
--- src/triangle.js
+++ src/triangle.js
@@ -1,47 +1,53 @@
 Triangle.prototype.checkType = function() {
+  // checks
+  // each input is a number, or a string with a number
+  // each input is > 0
+  // each side must be less than the sum of the other two
+  // figure out which triangle you have
 
-  if
-  ((isNaN(this.side1) === true ) ||
-  ((isNaN(this.side2) === true )) ||
-  (( isNaN(this.side3) === true ))) {
-    return "this string does not contain a number"
 
-  } else if
-  ((typeof this.side1 === "string" ) ||
-  (typeof this.side2 === "string" ) ||
-  ( typeof this.side3 === "string" )) {
-    return "this is string cheese"
+  // if
+  // ((isNaN(this.side1) === true ) ||
+  // ((isNaN(this.side2) === true )) ||
+  // (( isNaN(this.side3) === true ))) {
+  //   return "this string does not contain a number"
 
-  } else if
-  ((parseInt(this.side1) <= 0 ) ||
-  (parseInt(this.side2) <= 0 ) ||
-  (parseInt(this.side3) <= 0 )) {
-    return "this is a negative number"
+  // } else if
+  // ((typeof this.side1 === "string" ) ||
+  // (typeof this.side2 === "string" ) ||
+  // ( typeof this.side3 === "string" )) {
+  //   return "this is string cheese"
 
-  } else if
-  ((this.side1 > (this.side2 + this.side3)) ||
-  (this.side2 > (this.side1 + this.side3)) ||
-  (this.side3 > (this.side1 + this.side2))) {
-    return "not a triangle"
+  // } else if
+  // ((parseInt(this.side1) <= 0 ) ||
+  // (parseInt(this.side2) <= 0 ) ||
+  // (parseInt(this.side3) <= 0 )) {
+  //   return "this is a negative number"
 
-  } else if
-  ((this.side1 !== this.side2) &&
-  (this.side1 !== this.side3) &&
-  (this.side2 !== this.side3)){
-    return "scalene triangle"
+  // } else if
+  // ((this.side1 > (this.side2 + this.side3)) ||
+  // (this.side2 > (this.side1 + this.side3)) ||
+  // (this.side3 > (this.side1 + this.side2))) {
+  //   return "not a triangle"
 
-  } else if
-  ((this.side1 === this.side2) &&
-  (this.side1 === this.side3) &&
-  (this.side2 === this.side3)) {
-    return "equilateral triangle";
+  // } else if
+  // ((this.side1 !== this.side2) &&
+  // (this.side1 !== this.side3) &&
+  // (this.side2 !== this.side3)){
+  //   return "scalene triangle"
 
-  } else if
-  ((this.side1 === this.side2) ||
-  (this.side1 === this.side3) ||
-  (this.side2 === this.side3)) {
-    return "isosceles triangle";
-  }
+  // } else if
+  // ((this.side1 === this.side2) &&
+  // (this.side1 === this.side3) &&
+  // (this.side2 === this.side3)) {
+  //   return "equilateral triangle";
+
+  // } else if
+  // ((this.side1 === this.side2) ||
+  // (this.side1 === this.side3) ||
+  // (this.side2 === this.side3)) {
+  //   return "isosceles triangle";
+  // }
 };
 
 
@@ -49,4 +55,25 @@ export function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
+}
+
+// addSides :: triangleObject -> number
+// function receives a triangle, returns the sum of the sides
+export function addSides(triangle) {
+  var a = triangle.side1;
+  var b = triangle.side2;
+  var c = triangle.side3;
+
+  return a + b + c;
+}
+
+// multiplySides :: triangleObject -> number
+// function receives a triangle, returns the product of the sides
+// eslint-disable-next-line no-unused-vars
+export function multiplySides(triangle) {
+  var a = triangle.side1;
+  var b = triangle.side2;
+  var c = triangle.side3;
+
+  return a * b * c;
 }
